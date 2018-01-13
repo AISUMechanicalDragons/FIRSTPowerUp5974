c18466e6a38628971a7dd604125f0fcc6b057c51
Quantumplatr <etc@unknownstudio.com>
2018-01-13 11:05:05 -0700

    Arcade drive hopefully fully functional. Within function created

--- FIRSTPowerUp/src/org/usfirst/frc/team5974/robot/Robot.java
+++ FIRSTPowerUp/src/org/usfirst/frc/team5974/robot/Robot.java
@@ -291,6 +291,7 @@ public class Robot extends IterativeRobot {
 		}
 	}
 
+	
 	public void joystickDeadZone() { //dead zone for joysticks
 		if (joystickLXAxis <= 0.15 && joystickLXAxis <= -0.15) {
 			joystickLXAxis = 0;
