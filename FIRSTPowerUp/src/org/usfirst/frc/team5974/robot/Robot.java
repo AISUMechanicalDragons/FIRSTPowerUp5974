6096d69a40ab89e600c74dda2a860048a24234cb
Thomas Stay <thomasstay@gmail.com>
2018-01-17 17:35:37 -0700

    Converts the dPad into the gyro (Clockwise vs Counterclockwise)

--- FIRSTPowerUp/src/org/usfirst/frc/team5974/robot/Robot.java
+++ FIRSTPowerUp/src/org/usfirst/frc/team5974/robot/Robot.java
@@ -333,7 +333,7 @@ public class Robot extends IterativeRobot {
 
 		//d-pad/POV turns
 		if (dPad != -1) {
-			dPad = 360 - dPad; //Converts the dPad, a clockwise rotation, into the Gyro, a counterclockwise rotation.
+			dPad = 360 - dPad; //Converts the dPad, a clockwise rotation, into the gyro, a counterclockwise rotation.
 			rotateTo(dPad);
 		}
 		
