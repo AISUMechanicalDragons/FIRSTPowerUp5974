6a7e9f56d28d067ab34a1c58a0efcc4af620da97
Quantumplatr <etc@unknownstudio.com>
2018-02-10 11:29:37 -0700

    Controller variables moved to remote

--- FIRSTPowerUp/src/org/usfirst/frc/team5974/robot/Robot.java
+++ FIRSTPowerUp/src/org/usfirst/frc/team5974/robot/Robot.java
@@ -112,52 +112,8 @@ public class Robot extends IterativeRobot {
 	Spark motorGL = new Spark(4);
 	Spark motorGR = new Spark(5);
 	
-	//Variables we're using
-	Joystick controller = new Joystick(0);			//controller
 	ADIS16448_IMU IMU = new ADIS16448_IMU();		//imu: accelerometer and gyro
-
 	
-	double joystickLXAxis;			//left joystick x-axis
-	double joystickLYAxis;			//left joystick y-axis
-	double joystickRXAxis;			//right joystick x-axis
-	double joystickRYAxis;			//right joystick y-axis
-	double triggerL;				//left trigger
-	double triggerR;				//right trigger
-	boolean bumperL;				//left bumper
-	boolean bumperR;				//right bumper
-	boolean buttonX;				//x button
-	boolean buttonY;				//y button
-	boolean buttonA;				//a button
-	boolean buttonB;				//b button
-	int dPad;					    //d-pad
-	boolean joystickLPress;		    //left joystick button press
-	boolean joystickRPress;		    //right joystick button press
-	boolean buttonStart;			//start button
-	boolean buttonBack;			    //back button
-	
-	/**Button ports, however, do start at 1 with one being the 'A' button. - Thomas*/
-	int portButtonX = 3;
-	int portButtonY = 4;
-	int portButtonA = 1;
-	int portButtonB = 2;
-	
-	int portJoystickLPress = 9;
-	int portJoystickRPress = 10;
-	
-	int portJoystickLXAxis = 0;
-	int portJoystickLYAxis = 1;
-	int portJoystickRXAxis = 4;
-	int portJoystickRYAxis = 5;
-	
-	int portTriggerL = 2;
-	int portTriggerR = 3;
-	int portBumperL = 5;
-	int portBumperR = 6;
-	
-	int portButtonBack = 7;
-	int portButtonStart = 8;
-	
-	int portDPad = 0;
 	
 	double angleToForward = 0;
 	
